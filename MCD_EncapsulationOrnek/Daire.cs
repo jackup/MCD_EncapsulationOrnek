c937ce10107d7c751aca1e789e10446cbab2846f
jackup <yk.58@hotmail.com>
2021-11-29 20:12:42 +0300

    code completed

---
+++ MCD_EncapsulationOrnek/Daire.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_EncapsulationOrnek
+{
+    class Daire
+    {
+        //private double pi = 3;
+        public double pi { get; set; }
+
+        public double radius { get; set; }
+        public double Radius
+        {
+            get
+            {
+                if (radius > 0d)
+                {
+                    this.radius = radius;
+                }
+                return radius;
+            }
+            
+        }
+        public Daire()
+        {
+            pi = 3;
+        }
+        public Daire(double userPi, double radius)
+        {
+            pi = userPi;
+            this.radius = radius;
+        }
+
+        public void Cevre()
+        {
+            double cevre = 2 * pi * radius;
+            Console.WriteLine("Dairenin çevresi : " + cevre);
+        }
+        public void Alan()
+        {
+            double alan = radius * radius * pi;
+            Console.WriteLine("Dairenin alanı : " + alan);
+        }
+
+    }
+}
--- MCD_EncapsulationOrnek/Program.cs
+++ MCD_EncapsulationOrnek/Program.cs
@@ -10,6 +10,21 @@ namespace MCD_EncapsulationOrnek
     {
         static void Main(string[] args)
         {
+            //Daire isimli bir class'a aşağıdaki şekilde nesne oluşturma kuralları uygulayınız.
+            //Pi sayısı olarak bir özelik tutunuz. Bu özelliğin değerini 3 atayınız.
+            //Pi sayı değerini dışardan gönderiniz.
+            //Bu dairenin çevresini ve alanını hesaplayınız.
+
+
+            Daire d1 = new Daire();
+            d1.radius = 50;
+            d1.Alan();
+            d1.Cevre();
+
+            Daire d2 = new Daire(3.14, 50);
+            d2.Alan();
+            d2.Cevre();
+
         }
     }
 }
